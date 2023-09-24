75a98fe22c79cc932cd2af868ae5e0742e115e69
root <root@ip-172-26-6-244.ec2.internal>
2023-09-24 21:37:12 +0000

    agent scripts

---
+++ input.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+
+
+
+read -p "What is your name ? " NAME
+read -p "Are you taking the class with utrains? " c
+
+if [ $c = yes ]
+ then
+ echo "Good job $NAME !! keep enjoying and change your life Amen!!"
+else
+  echo "thats not good $NAME Please chech the website immediatly and enroll ( utains.org)"
+
+fi
+
+
+
+#echo " your name is: $NAME and you answer $c to the utains class taking" 
